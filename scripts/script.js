d3c40a3f2cd9d4e47893674d4b2d898aba6f6eb7
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-09 14:45:17 +0100

    Update

---
+++ scripts/script.js
@@ -0,0 +1,67 @@
+//Words
+let words = [
+    'witchery',
+    'wizzard',
+    'dragon',
+    'fairy',
+    'unicorn',
+    'moonstone',
+    'elf',
+    'magic',
+    'dwarf',
+    'fantasy',
+    'cauldron',
+    'wand',
+    'potion',
+    'troll',
+    'legends',
+    'tales',
+    'yggdrasil',
+    'princess',
+    'castle',
+    'knight'
+]
+
+//Variables
+let answer = '';
+let maxAnswers = 10;
+let mistakes = 0;
+let guessed = [];
+
+//Random Word
+function randomWord() {
+    answer = words[Math.floor(Math.random() * words.length)];
+}
+randomWord();
+
+//Keyboard
+function alphabetButtons() {
+    let letterButtons = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+        `
+        <button class = 'btn' id = '`+ letter + `' onClick = 'handleGuess('` + letter + `')'>
+            `+ letter + `
+        </button>
+        `
+    );
+
+    document.getElementById('keyboard').innerHTML = letterButtons;
+}
+alphabetButtons();
+
+
+
+//Type writer effect for the h1
+let speed = 200;
+let i = 0;
+
+let h = document.getElementById('typeWriter');
+let text = 'Hangman Game';
+
+function typeWriterEffect() {
+    if (i < text.length) {
+        h.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriterEffect, speed);
+    }
+}
+typeWriterEffect();
