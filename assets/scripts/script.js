d77e6f4a5f8451d7e54bc7cb859ff5dfca3ea8d1
nataliacherman08 <cherman.natalia08@gmail.com>
2021-02-11 17:02:34 +0100

    Update

---
+++ assets/scripts/script.js
@@ -0,0 +1,137 @@
+//1) Create an array of words (const because we will not change this array)
+const words = [
+    'witchery',
+    'wizzard',
+    'dragon',
+    'fairy',
+    'unicorn',
+    'moonstone',
+    'elf',
+    'magic',
+    'dwarf',
+    'fantasy',
+    'cauldron',
+    'wand',
+    'potion',
+    'troll',
+    'legends',
+    'tales',
+    'yggdrasil',
+    'princess',
+    'castle',
+    'knight',
+    'ghost',
+    'crystal',
+    'tarot',
+    'wendigo',
+    'goblin',
+    'werewolf',
+    'vampire',
+    'elixir'
+];
+
+
+//2) Declare Variables (It's important to declare the variables outside the code blocs for the scope)
+let answer = '';
+let maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
+
+
+//3) Pick a secretWord
+function randomWord() {
+    answer = words[Math.floor(Math.random() * words.length)];
+
+    console.log(answer); //To show the answer in the console
+}
+
+
+
+//Update the maximum permissible wrong answers 
+document.getElementById('maxWrong').innerHTML = maxWrong;
+
+
+
+//4) Generate a keyboard dynamically by JS
+function generateButtons() {
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+        `
+      <button
+        class='alphabet'
+        id='` + letter + `'
+        onClick="handleGuess('` + letter + `')"
+      >
+        ` + letter + `
+      </button>
+    `).join('');//To delete the commas
+
+    document.getElementById('keyboard').innerHTML = buttonsHTML;//To insert this keyboard in HTML
+}
+
+// ====The function of the Game========
+function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+    document.getElementById(chosenLetter).setAttribute('disabled', true);
+
+    if (answer.indexOf(chosenLetter) >= 0) {
+        guessedWord();
+        checkIfGameWon();
+    } else if (answer.indexOf(chosenLetter) === -1) {
+        mistakes++;
+        updateMistakes();
+        checkIfGameLost();
+        updatePicture();
+    }
+}
+
+
+//1) Function that add 1 to mistake
+function updateMistakes() {
+    document.getElementById('mistakes').innerHTML = mistakes;
+}
+
+
+function updatePicture() {
+    document.getElementById('hangedMan').src = 'images' + mistakes + 'hanged.png';
+}
+
+
+//Function to check if the user won
+function checkIfGameWon() {
+    if (wordStatus === answer) {
+        document.getElementById('keyboard').innerHTML = 'Congratulations, play again ?';
+    }
+}
+
+//Function to check if the user lose
+function checkIfGameLost() {
+    if (mistakes === maxWrong) {
+        document.getElementById('blanks').innerHTML = 'The secret word as : ' + answer;
+        document.getElementById('keyboard').innerHTML = 'Sorry, maybe the next time.';
+    }
+}
+
+function guessedWord() {
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+    document.getElementById('blanks').innerHTML = wordStatus;
+}
+
+
+
+//Reset
+function reset() {
+    mistakes = 0;
+    guessed = [];
+    randomWord();
+    guessedWord();
+    updateMistakes();
+    generateButtons();
+    document.getElementById('hangedMan').src = 'images/hanged6.png';
+}
+
+
+//Launch the functions
+randomWord();
+generateButtons();
+guessedWord();
